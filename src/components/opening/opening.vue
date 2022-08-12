9f94c72cec348491b9cfee39b98df2accd9fed28
ikenk <ikenkhj@gmail.com>
2022-08-12 23:38:25 +0800

    Initial commit

---
+++ src/components/opening/opening.vue
@@ -0,0 +1,184 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    mounted() {
+        setTimeout(this.goToHomePage, 2000)
+    },
+    methods: {
+        goToHomePage() {
+            this.$router.push({name: 'home'})
+        }
+    }
+})
+
+</script>
+
+<template>
+    <!-- 开场动画部分 -->
+    <div class="firpage"  id="firstpage" >
+        <div class="startani">
+            <img src="../assets/imgs/ttl_logo01_1.svg" class="img1">
+            <img src="../assets/imgs/ttl_logo01_3.svg" class="img2">
+            <img src="../assets/imgs/ttl_logo01_sp.svg" class="img3">
+            <div class="startleft">
+            </div>
+            <div class="startright">
+                <img src="../assets/imgs/bg_opening00.jpg" class="rightimage0">
+                <img src="../assets/imgs/bg_opening01.jpg" class="rightimage1">
+                <img src="../assets/imgs/bg_opening02.jpg" class="rightimage2">
+                <img src="../assets/imgs/bg_opening03.jpg" class="rightimage3">
+                <img src="../assets/imgs/img_noise01.png" class="rightimg1">
+                <img src="../assets/imgs/img_noise02.png" class="rightimg2">
+                <img src="../assets/imgs/img_noise03.png" class="rightimg3">
+                <img src="../assets/imgs/img_noise04.png" class="rightimg4">
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+/* 1.0 firstpage整体布局 */
+.firpage{
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    z-index: -10;
+    /* background: black; */
+    /* background-image: url("./images/img_noise01.png"); */
+}
+
+/* 1.1总体窗口布局 */
+.startani{
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    display: flex;
+    flex-flow: row nowrap;
+}
+
+/* 左右窗口布局 */
+.startleft{
+    position: relative;
+    width: 50%;
+    height: 100%;
+    /* background-color: aqua; */
+}
+.startright{
+    position: relative;
+    width: 50%;
+    height: 100%;
+    background-color:white;
+}
+
+/* steinsgate图标样式 */
+img[class^="img"]{
+    position: absolute;
+    width: 35%;
+    top: 50%;
+    left: 50%;
+    transform:translate(-80%,-50%);
+    z-index: 10;
+}
+
+
+/* rightimg样式 */
+img[class^="rightimg"]{
+    position: absolute;
+    height: 100%;
+    right:0;
+}
+
+
+/* rightimages样式 */
+img[class^="rightimage"]{
+    position: absolute;
+    height: 100%;
+}
+.rightimage0{
+    z-index: 4;
+}
+.rightimage1{
+    z-index: 3;
+}
+.rightimage2{
+    z-index: 2;
+}
+.rightimage3{
+    z-index: 1;
+}
+
+
+/* 闪烁动画 */
+@-webkit-keyframes twinkling{ 
+    0%{ 
+        opacity: 0.5; 
+    } 
+    100%{ 
+        opacity: 1; 
+    } 
+} 
+@keyframes twinkling{ 
+    0%{ 
+        opacity: 0.5; 
+    } 
+    100%{ 
+        opacity: 1; 
+    } 
+}
+
+/* 移位动画 */
+@-webkit-keyframes move{ 
+    100%{ 
+        transform: translate(-2%,-2%); 
+    } 
+} 
+@keyframes move{ 
+    100%{ 
+        transform: translate(0,-2%); 
+    } 
+}
+
+/* 各部分闪烁和为宜 */
+.img1{
+    animation: twinkling 0.1s 6 ;
+}
+.img2{
+    animation: twinkling 0.1s 0.2s 6 ;
+}
+.img3{
+    animation: twinkling 0.1s 0.2s 6 ;
+}
+.rightimage0{
+    animation: twinkling 0.1s 0.2s 5 , move 0.1s 0.2s;
+}
+.rightimage1{
+    animation: twinkling 0.1s 0.2s 3 ;
+}
+.rightimage2{
+    animation: twinkling 0.1s 0.2s 3 , move 0.1s 0.5s 5;
+}
+.rightimage3{
+    /* animation: twinkling 0.1s 0.1s 5 ; */
+    animation: move 0.1s 0.6s ;
+}
+.rightimg1{
+    animation: twinkling 0.1s  6 ;
+}
+.rightimg2{
+    animation: twinkling 0.1s  0.2s 6 ;
+}
+.rightimg3{
+    animation: twinkling 0.1s  0.1s 6 ;
+}
+.rightimg4{
+    animation: twinkling 0.1s 0.2s 6 ;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+//引入组件
+import 
+
+
+createApp(App).mount('#app')
