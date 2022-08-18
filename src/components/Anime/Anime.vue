71e1cedcfe1f9b727271a90152b90a9f3a23e9f9
ikenk <ikenkhj@gmail.com>
2022-08-19 01:06:47 +0800

    2

--- src/components/Anime/Anime.vue
+++ src/components/Anime/Anime.vue
@@ -1,167 +1,185 @@
 <template>
-    <div class="container">
-        <div class="bg_img" :style="{top: iScol + 'px'}" >
-            <div class="bg_longimg"></div>
-            <div class="bg_grayimg"></div>
-            <div class="bg_pointimg"></div>
-        </div>
-        <div class="contents_is_show">
-            <div class="contents">
-                <div class="top_img">
-                    <div class="top_img_left"></div>
-                    <div class="top_img_right"></div>
-                </div>
-                <div class="SCSC">
-                    <!-- <Switch v-model="value" @on-change="change" class="switch_timeline"/> -->
-                    <div class="alpha_timeline">
-                        <div class="story"></div>
-                        <div class="characters"></div>
-                        <div class="staff-cast"></div>
-                    </div>
-                    <div class="beta_timeline">
-                        <div class="story"></div>
-                        <div class="characters"></div>
-                        <div class="staff-cast"></div>
-                    </div>
-                </div>
-                <div class="product"></div>
-                <div class="footer"></div>
-            </div>
-        </div>
-        <div class="navbtn_is_show"></div>
-        <div class="nav"></div>
+  <div class="container">
+    <div class="bg_img" :style="{ top: iScol + 'px' }">
+      <div class="bg_longimg"></div>
+      <div class="bg_grayimg"></div>
+      <div class="bg_pointimg"></div>
     </div>
+    <div class="contents_is_show">
+      <div class="contents">
+        <div class="top_img">
+          <div class="top_img_left"></div>
+          <div class="top_img_right"></div>
+        </div>
+        <Row class="SCSC">
+            <Row class="beta_timeline">
+                <Row class="b_story">
+                    <Row class="b_st_title">
+                        <Col span="8" ></Col>
+                        <Col span="8" >
+                            <Row class="b_t_img"></Row>
+                        </Col>
+                        <Col span="8" ></Col>
+                    </Row>
+                    <Row class="b_st_body">
+                        <Row class="body_text"></Row>
+                        <Row class="body_img">
+                            <Row class="body_slideimg">                                                           
+                                <Col span="15" class="slideimg_left" >
+                                  <div class="leftimg">
+                                      <ul class="leftimg_moveup" >
+                                        <li><img src="/src/assets/images/img_story01_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story02_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story03_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story04_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story05_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story06_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story07_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story08_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story09_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story10_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story11_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story12_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story13_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story14_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story15_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story16_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story17_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story18_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story19_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story20_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story21_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story22_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story23_01.png.png"></li>
+                                        <li><img src="/src/assets/images/img_story24_01.png.png"></li>
+                                      </ul>
+                                  </div>
+                                </Col>
+                                <Col span="6" class="slideimg_middle">
+                                    <div class="midimg">
+                                        <ul class="midimg_movedown">
+                                            <li><img src="/src/assets/images/img_story01_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story02_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story03_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story04_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story05_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story06_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story07_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story08_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story09_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story10_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story11_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story12_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story13_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story14_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story15_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story16_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story17_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story18_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story19_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story20_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story21_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story22_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story23_02.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story24_02.png.png"></li>
+                                        </ul>
+                                    </div>
+                                </Col>
+                                <Col span="3" class="slideimg_right">
+                                    <div class="rightimg">
+                                        <ul class="rightimg_moveup">
+                                            <li><img src="/src/assets/images/img_story01_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story02_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story03_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story04_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story05_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story06_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story07_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story08_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story09_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story10_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story11_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story12_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story13_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story14_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story15_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story16_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story17_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story18_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story19_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story20_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story21_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story22_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story23_03.png.png"></li>
+                                            <li><img src="/src/assets/images/img_story24_03.png.png"></li>
+                                        </ul>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Row>
+                        <Row class="b_st_footer">
+                          <button @click='getRef'>切换集数</button>
+                        </Row>
+                        <Row class="num_nav"></Row>
+                    </Row>
+                <Row class="b_characters"></Row>
+                <Row class="b_footer"></Row>
+                </Row>
+            </Row>
+            <Row class="alpha_timeline">
+                <Row class="a_story">
+                    <Row class="a_st_title"></Row>
+                    <Row class="a_st_body"></Row>
+                    <Row class="a_st_footer"></Row>
+                </Row>
+                <Row class="a_characters"></Row>
+                <Row class="a_footer"></Row>
+            </Row>
+        </Row>
+        <br />
+        <Row class="product">
+        </Row>
+        <br />
+        <Row class="footer">
+        </Row>
+      </div>
+    </div>
+    <div class="navbtn_is_show"></div>
+    <div class="nav"></div>
+  </div>
 </template>
 
 
 <script>
 export default {
-    name:'Anime',
-    data(){
-        return{
-            iScol:'0',
-            value: false,//布尔值
-        }
+  name: "Anime",
+  data() {
+    return {
+      iScol: "0",
+      value: false, //布尔值
+      width:'0',
+      height:'0',
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.scrollToTop);
+  },
+  methods: {
+    scrollToTop() {
+      var scrollTop =
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop;
+      console.log(scrollTop);
+      this.iScol = -1.8 * scrollTop;
     },
-    mounted () {
-        window.addEventListener('scroll', this.scrollToTop)
+    change(){
+
     },
-    methods:{
-        scrollToTop() {
-            var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-            console.log(scrollTop)
-            this.iScol = -1.8*scrollTop
-        },
-        change (status) {
-                this.$Message.info('开关状态：' + status);
-                //value与status无直接关系，即使注释掉上面一句也可以打印出value
-                console.log(this.value);
-                
-        },
     }
-
-
-
 }
 </script>
 
 <style scoped>
-/* 0.通用样式 */
-*{
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-    -webkit-box-sizing: border-box;
-}
-
-
-/* 1.背景样式 */
-.bg_img{
-    position: absolute;
-    width: 100vw;
-    height: 50000px;
-    z-index: -1000;
-}
-
-.bg_img .bg_longimg{
-    position: absolute;
-    top:0;
-    left: 0;
-    width: 100vw;
-    height: 50000px;
-    background-image: url("/src/assets/images/bg_mainbg01.jpg");
-    background-size: contain;
-    background-repeat: repeat-y;
-    z-index: -999;
-}
-
-.bg_img .bg_grayimg{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 50000px;
-    background-color: rgba(0,0,0,0.85);
-    z-index: -998;
-}
-
-.bg_img .bg_pointimg{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 50000px;
-    background-image: url("/src/assets/images/whitepoint.png");
-    opacity: 0.15;
-    z-index: -997;
-}
-
-/* 2.Top图样式 */
-.top_img{
-    position: relative;
-    width: 100vw;
-    height: 100vh;
-    background-color:white;
-    /*opacity: 0.9;*/
-}
-/* 2.1 Top-left图样式 */
-.top_img .top_img_left{
-    position: absolute;
-    top:0;
-    left: 0;
-    width: 40vw;
-    height: 100vh;
-    background-image: url("/src/assets/images/ttl_logo01_sp.svg");
-    background-repeat: no-repeat;
-    background-position: center;
-}
-/* 2.2 Top-right图样式 */
-.top_img .top_img_right{
-    position: absolute;
-    top:0;
-    right:0;
-    width: 60vw;
-    height: 100vh;
-    background-image: url("/src/assets/images/bg_hero00.png");
-    background-position: right center;
-    background-size: cover;
-    background-repeat: no-repeat;
-}
-
-
-/*3.story/character/staff/cast样式*/
-.switch_timeline{
-    width: 100vw;
-    height: 100vh;
-    
-}
-
-.alpha_timeline{
-    position: absolute;
-    width: 100vw;
-    height: 100vh;
-    background-color: #fff;
-}
-
-
+@import url("./Anime.css");
 </style>
