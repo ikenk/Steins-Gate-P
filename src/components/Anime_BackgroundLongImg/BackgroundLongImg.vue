0d791b1102e6fb24698d553cd196d5dfe50c9ed6
ikenk <ikenkhj@gmail.com>
2022-08-29 21:47:33 +0800

    25

--- src/components/Anime_BackgroundLongImg/BackgroundLongImg.vue
+++ src/components/Anime_BackgroundLongImg/BackgroundLongImg.vue
@@ -39,7 +39,7 @@ export default {
   .bg_img {
     position: absolute;
     width: 100vw;
-    height: 50000px;
+    height: 1300vw;
     z-index: -1000;
   }
   
@@ -48,7 +48,7 @@ export default {
     top: 0;
     left: 0;
     width: 100vw;
-    height: 50000px;
+    height: 1300vw;
     background-image: url("/src/assets/images/bg_mainbg01.jpg");
     background-size: contain;
     background-repeat: repeat-y;
@@ -60,7 +60,7 @@ export default {
     top: 0;
     left: 0;
     width: 100vw;
-    height: 50000px;
+    height: 1300vw;
     background-color: rgba(0, 0, 0, 0.9);
     z-index: -998;
   }
@@ -70,7 +70,7 @@ export default {
     top: 0;
     left: 0;
     width: 100vw;
-    height: 50000px;
+    height: 1300vw;
     background-image: url("/src/assets/images/whitepoint.png");
     opacity: 0.15;
     z-index: -997;
